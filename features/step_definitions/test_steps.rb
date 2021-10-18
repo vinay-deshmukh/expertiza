cb45fb1eb2a46d52891e2c6990809860b581571e
Vinay Deshmukh <vinay_deshmukh@outlook.com>
2021-10-18 17:33:43 -0400

    WIP: database in cucumber is not the same in RSpec/Rails

--- features/step_definitions/test_steps.rb
+++ features/step_definitions/test_steps.rb
@@ -1,3 +1,4 @@
+require "byebug"
 #Navigate to google.co.uk
 Given(/^I am on the Google homepage$/) do
     visit 'https://www.google.co.uk/'
@@ -27,8 +28,37 @@ end
 
 Given(/^I am on the assignment (\d+) edit page$/) do |assignment_id|
 #   pending # Write code here that turns the phrase above into concrete actions
-#   visit "/assignments/#{assignment_id}/edit"
-  visit "/assignments/95/edit"
+    instructor = create(:instructor, id: 6)
+
+    # login_as("instructor6")
+
+#   def stub_current_user(current_user, current_role_name = 'Student', current_role)
+#     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(current_user) if defined?(session)
+#     allow_any_instance_of(ApplicationController).to receive(:current_role_name).and_return(current_role_name)
+#     allow_any_instance_of(ApplicationController).to receive(:current_role).and_return(current_role)
+#     # Also pop this stub user into the session to support the authorization helper
+
+#     # Check if session is defined to differentiate between controller and non-controller tests.
+#     # This is required as the session variable is only defined for controller specs.
+#     # Other kinds of specs(feature specs,etc) use an internal rack.session that cannot be interacted with.
+#     session[:user] = current_user if defined?(session)
+#   end
+
+    # stub_current_user(instructor, instructor.role.name, instructor.role)
+    assignment = create(:assignment,
+        id: assignment_id,
+        name: 'test assignment',
+        instructor_id: instructor.id,
+        staggered_deadline: true,
+        directory_path: 'same path',
+        participants: [build(:participant)],
+        teams: [build(:assignment_team)],
+        course_id: 1
+    )
+    puts Assignment.all
+    # byebug
+    visit "/assignments/#{assignment_id}/edit"
+    # visit "/assignments/95/edit"
 #   visit "/assignments/"
 end
 
--- features/support/env.rb
+++ features/support/env.rb
@@ -3,7 +3,35 @@ require 'rubygems'
 require 'capybara'
 require 'capybara/dsl'
 require 'rspec'
+require 'factory_bot_rails'
+require 'cucumber/rspec/doubles'
+# require File.expand_path("../../spec/rails_helper", __FILE__)
 # require 'byebug'
+# require 'features/support/database_cleaner'
+
+# DatabaseCleaner.clean_with(:deletion) # clean once, now
+# DatabaseCleaner.strategy = :transaction
+# Cucumber::Rails::Database.javascript_strategy = :deletion
+
+# require 'cucumber/rails/active_record'
+# require 'cucumber/rails/world'
+
+Cucumber::Rails::World.use_transactional_fixtures = false
+Cucumber::Rails::Database.autorun_database_cleaner = false
+DatabaseCleaner.strategy = :truncation
+Cucumber::Rails::Database.javascript_strategy = :truncation
+
+Before do
+  DatabaseCleaner.clean
+end
+# begin
+#   require 'database_cleaner'
+#   require 'database_cleaner/cucumber'
+
+#   DatabaseCleaner.strategy = :truncation
+# rescue NameError
+#   raise "You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it."
+# end
 
 Capybara.run_server = true
 # For GUI-based firefox
@@ -22,3 +50,5 @@ module Helpers
   end
 end
 World(Capybara::DSL, Helpers)
+World(FactoryBot::Syntax::Methods)
+World(Rails.application.routes.url_helpers)
